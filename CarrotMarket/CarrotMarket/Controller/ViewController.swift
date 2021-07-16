a156dff5659b8463d2fd95d9539a43550b8bb4c0
helloworldjay <sj.seungjin.baek@gmail.com>
2021-07-16 18:30:01 +0900

    [#2]feat: 앱 기본 구조 구현
    - 앱 기본 디렉토리 구현
    - 앱 아이콘 설정

---
+++ CarrotMarket/CarrotMarket/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarrotMarket
+//
+//  Created by Seungjin Baek on 2021/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
