32d6fdf81e04c76c7299c652f670ad1bf6a10281
helloworldjay <sj.seungjin.baek@gmail.com>
2021-07-17 23:49:07 +0900

    [#3]feat: Cell 구현을 위한 MainTableViewCell 생성
    - UITableViewCell을 생성 후 storyboard와 연결

---
+++ CarrotMarket/CarrotMarket/Cell/MainTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  MainTableViewCell.swift
+//  CarrotMarket
+//
+//  Created by Seungjin Baek on 2021/07/17.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var itemTitle: UILabel!
+    @IBOutlet weak var itemLocation: UILabel!
+    @IBOutlet weak var itemWrittenTime: UILabel!
+    @IBOutlet weak var itemPrice: UILabel!
+    @IBOutlet weak var itemLike: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
