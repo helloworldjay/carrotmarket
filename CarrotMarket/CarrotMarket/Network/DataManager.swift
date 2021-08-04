03965cff9876ff3254ca4f704419e40313c2add3
helloworldjay <sj.seungjin.baek@gmail.com>
2021-08-04 11:58:44 +0900

    [#3]feat: decoding을 위한 DataManager 구현
    - DataManager에 decodeJSONData 메소드 구현

---
+++ CarrotMarket/CarrotMarket/Network/DataManager.swift
@@ -0,0 +1,21 @@
+//
+//  DataManager.swift
+//  CarrotMarket
+//
+//  Created by Seungjin Baek on 2021/07/31.
+//
+
+import Foundation
+
+struct DataManager {
+    
+    func decodeJSONData<T: Decodable>(with data: Data) -> T? {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(DateFormatter.iso8601Full)
+        guard let model = try? decoder.decode(T.self, from: data) else {
+            return nil
+        }
+        return model
+    }
+    
+}
